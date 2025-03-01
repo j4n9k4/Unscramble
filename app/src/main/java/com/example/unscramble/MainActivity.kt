87f359af4bcb8209565b2b8ff766349df4b2b757
j4n9k4 <janukananayakkara@gmail.com>
2025-03-01 22:57:37 +0530

    Added a viewModel

--- app/src/main/java/com/example/unscramble/MainActivity.kt
+++ app/src/main/java/com/example/unscramble/MainActivity.kt
@@ -22,7 +22,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.unscramble.ui.GameScreen
 import com.example.unscramble.ui.theme.UnscrambleTheme
 
@@ -39,5 +41,13 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+@Preview(showSystemUi = true)
+@Composable
+fun GamePreview()
+{
+    UnscrambleTheme {
+        GameScreen()
     }
 }
